8b49be05bc92ca2f0401e0a4db00b3a9694e63b8
ahmad fauzan <fauzan.c4@gmail.com>
2022-04-08 20:28:38 +0700

    get dependency v2

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/fauzan264/app-first-module
 
 go 1.17
 
-require github.com/fauzan264/go-first-module v1.3.0
+require github.com/fauzan264/go-first-module/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 
-	go_first_module "github.com/fauzan264/go-first-module"
+	go_first_module "github.com/fauzan264/go-first-module/v2"
 )
 
 func main() {
 	for i := 0; i < 10; i++ {
-		fmt.Println(go_first_module.First(), " ", i)
+		fmt.Println(go_first_module.First(" Fauzan"), " ", i)
 	}
 }
